aad0e6fd1694b9f63eea90cdd38be9fb3438cea2
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-07-12 08:22:16 +0200

    GlobalStyle I created

---
+++ src/common/GlobalStyle.tsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyle = createGlobalStyle`
+
+html {
+    box-sizing: border-box;
+}
+*,
+::after,
+::before {
+    box-sizing: inherit;
+}
+
+body {
+    font-family: 'Inter', sans-serif;
+       width: 100%;
+    margin: 0;
+}
+
+`;
