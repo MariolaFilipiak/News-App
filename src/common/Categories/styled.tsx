979623304579ba6fb7e772ec3b61492731b19cd7
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-07-12 11:27:26 +0200

    Link to category I added

--- src/common/Categories/styled.tsx
+++ src/common/Categories/styled.tsx
@@ -1,16 +1,17 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Wrapper = styled.div`
-background-color: #303030;
-display: flex;
-flex-wrap: nowrap;
-flex-direction: row;
-justify-content: center;
-align-items:center;
-
+  background-color: #303030;
+  display: flex;
+  flex-wrap: nowrap;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 `;
-export const Name = styled.p`
-color: #ffffff;
-padding: 10px;
-margin: 5px;
+export const Name = styled(Link)`
+  color: #ffffff;
+  padding: 10px;
+  margin: 5px;
+  text-decoration: none;
 `;
