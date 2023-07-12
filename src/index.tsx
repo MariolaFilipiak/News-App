324f2d97283f10f8a4f37c0711ec85229b9ea12f
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-07-12 08:21:14 +0200

    tanstack i

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { GlobalStyle } from './common/GlobalStyle';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools />
+    <GlobalStyle/>
     <App />
-  </React.StrictMode>
+ </QueryClientProvider>
+
 );
