7ebfe2e8dbc72340734ead2ea4795dbc2f681364
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-07-12 08:17:39 +0200

    I created Country component

---
+++ src/features/Articles/Country/index.tsx
@@ -0,0 +1,30 @@
+import { Tag, Wrapper } from "./styled";
+
+const Country = () => {
+  return (
+    <Wrapper>
+      <Tag>Poland</Tag>
+      <Tag>Greece</Tag>
+      <Tag>US</Tag>
+      <Tag>Brazil</Tag>
+      <Tag>Argentina</Tag>
+      <Tag>Finland</Tag>
+      <Tag>Denmark</Tag>
+      <Tag>Spain</Tag>
+      <Tag>Italy</Tag>
+      <Tag>Japan</Tag>
+      <Tag>Ukraine</Tag>
+      <Tag>Slovakia</Tag>
+      <Tag>France</Tag>
+      <Tag> England</Tag>
+      <Tag> Scotland</Tag>
+      <Tag> Ireland</Tag>
+      <Tag> Germany</Tag>
+      <Tag> Belgium</Tag>
+      <Tag> Norway</Tag>
+      <Tag> Turkey</Tag>
+    </Wrapper>
+  );
+};
+
+export default Country;
---
+++ src/features/Articles/Country/styled.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-content: flex-start;
+  height: 400px;
+
+`;
+export const Tag = styled.div`
+  background-color: #e8e8e8;
+  color: #262626;
+  max-width:150px;
+  height: 40px;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 15px;
+`;
