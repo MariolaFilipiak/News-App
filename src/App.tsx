2cbe7b91575d55697613496ca924e7b60051c77c
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-07-12 08:16:30 +0200

    I added Articles and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Categories from "./common/Categories";
+import Footer from "./common/Footer";
 import Navigate from "./common/Navigate";
+import Articles from "./features/Articles/Articles";
 
 function App() {
   return (
-   <>
-   <Navigate/>
-   <Categories/>
-   </>
+    <>
+      <Navigate />
+      <Categories />
+      <Articles />
+      <Footer/>
+    </>
   );
 }
 
