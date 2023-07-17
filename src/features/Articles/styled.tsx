086bfc2a1d8494434a94e7c50d412439000cb2fd
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-07-17 18:11:15 +0200

    max-width added infoBox , Box and MoreButton created

--- src/features/Articles/styled.tsx
+++ src/features/Articles/styled.tsx
@@ -6,31 +6,46 @@ export const Section = styled.section`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   column-gap: 30px;
-  margin-top: 50px;
+  margin-top: 30px;
   margin-bottom: 50px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}) {
+    grid-template-columns: 1fr;
+    margin-top: 10px;
+  }
 `;
 export const Tile = styled.div`
   height: 100%;
-  display: grid;
-  grid-gap:30px;
+  display: flex;
+  flex-direction: column;
   padding: 10px;
   gap: 10px;
-  border-bottom: 1px solid grey;
-  position: relative ;
+  border-bottom: 1px solid ${({ theme }) => theme.color.cornFlowerLilac};
 `;
 
 export const Title = styled.h2`
-  color: #262626;
-  margin: 0;
+  color: ${({ theme }) => theme.color.mineShaft};
   line-height: 1.4;
-  letter-spacing: 0.4;
+  letter-spacing: 0.5px;
   font-size: 25px;
+  margin-top: 16px;
 
-//height: 200px;
+  @media (max-width: ${({ theme }) => theme.breakpoint.min}) {
+    font-size: 20px;
+  }
 `;
 
-export const Date = styled.h3`
-  color: #a9a9a9;
+export const InfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  gap: 10px;
+`;
+
+export const ArticleDate = styled.h3`
+  color: ${({ theme }) => theme.color.silverChalice};
   margin: 0;
   line-height: 1.4;
   letter-spacing: 0.4;
@@ -39,25 +54,42 @@ export const Date = styled.h3`
 `;
 
 export const Description = styled.h4`
-  color: #616161;
+  color: ${({ theme }) => theme.color.doveGray};
   margin: 0;
   line-height: 1.4;
-  letter-spacing: 0.4;
+  letter-spacing: 0.8px;
   font-size: 20px;
   font-weight: normal;
+  @media (max-width: ${({ theme }) => theme.breakpoint.min}) {
+    font-size: 15px;
+  }
 `;
 
 export const Image = styled.img`
   width: 100%;
-  filter: grayscale(100%);
+  aspect-ratio: 3 / 2;
 `;
 export const Button = styled.div`
-text-align: center;
-margin:0 auto;
-margin-bottom: 50px;
-background-color:#e8e8e8;
-width: 100px;
-padding: 5px;
-color:262626;
-letter-spacing: 1.4;
+  text-align: center;
+  margin: 0 auto;
+  margin-bottom: 50px;
+  background-color: ${({ theme }) => theme.color.ebb};
+  width: 100px;
+  padding: 5px;
+  color: 262626;
+  letter-spacing: 1.4;
+`;
+export const Box = styled.div`
+  text-decoration: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const MoreButton = styled.a`
+  display: flex;
+  &:hover {
+    opacity: 0.5s;
+    transform: scale(1.03);
+    cursor: pointer;
+  }
 `;
