4bc4ec9b579f01997d4160000a481b148557f5d4
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-07-18 09:17:11 +0200

    I removed Country and limit and I added MoreButton, searchParams,searchQuery and I changed names for nytimes and I added Error,NoResults and Loading pages

--- src/common/Categories/CategoryPage.tsx
+++ src/common/Categories/CategoryPage.tsx
@@ -1,63 +1,92 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import { useParams } from "react-router-dom";
-import Country from "../../features/Articles/Country/Country";
+import { useParams, useSearchParams } from "react-router-dom";
+import Tags from "./Tags";
 import {
-  Button,
-  Date,
+  ArticleDate,
   Description,
   Image,
   Section,
   Tile,
   Title,
+  InfoBox,
+  Box,
+  MoreButton,
 } from "../../features/Articles/styled";
 import { getCategory } from "../getData";
+import Error from "../StatusPage/Error/Error";
+import Loading from "../StatusPage/Loading/Loading";
+import NoResult from "../StatusPage/NoResult/NoResult";
+import { searchQueryParamName } from "../Navigate/SearchBar/searchQueryParamName";
 
 const CategoryPage = () => {
-  const { category } = useParams<{ category: string }>();
-  const [limit, setLimit] = useState(10);
-  const { data, isLoading, isError } = useQuery(["articles", category], () =>
-    getCategory(category || "")
+  const [searchParams] = useSearchParams();
+  const { category } = useParams();
+  const searchQuery = searchParams.get(searchQueryParamName);
+
+  const { data, isLoading, isError } = useQuery(
+    ["articles", category, searchQuery],
+    () => getCategory(category || "", searchQuery || "")
   );
 
-  const articles = data?.articles || [];
-  const handleShowMore = () => {
-    setLimit((prevLimit) => prevLimit + 10);
-  };
-
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Loading />;
   }
 
   if (isError) {
-    return <div>Error occurred while fetching data.</div>;
+    return <Error />;
   }
-
+  if (data.length === 0) {
+    return <NoResult />;
+  }
   return (
     <>
       <Section>
-        <Country />
-        {articles.slice(0, limit).map((article: any) => (
-          <Tile key={article.id}>
-            <Title>{article.title}</Title>
-            <Date>{article.publishedAt}</Date>
-            {article.urlToImage ? (
-              <Image src={article.urlToImage} alt={article.title} />
+        <Tags />
+        {data?.map((article: any) => (
+          <Tile key={article._id}>
+            {article.multimedia && article.multimedia.length > 0 ? (
+              <Image
+                src={`https://www.nytimes.com/${article.multimedia[0].url}`}
+                alt={article.headline.main}
+              />
             ) : (
               <Image
                 src="https://t4.ftcdn.net/jpg/03/24/14/35/360_F_324143588_Jk9uwkSlhuSEyrGWkuQT7MM6mFbCayIj.jpg"
                 alt="noImage"
               />
             )}
-            <Description>{article.description}</Description>
+            <InfoBox>
+              <Title>{article.headline.main}</Title>
+              <ArticleDate>
+                {new Date(article.pub_date).toLocaleDateString("pl-PL", {
+                  day: "2-digit",
+                  month: "2-digit",
+                  year: "numeric",
+                })}
+              </ArticleDate>
+
+              <Description>{article.abstract}</Description>
+            </InfoBox>
+            <Box>
+              <MoreButton
+                href={article.web_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="#ad615ac1"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                </svg>
+              </MoreButton>
+            </Box>
           </Tile>
         ))}
       </Section>
-      {limit < articles.length && (
-        <>
-          <Button onClick={handleShowMore}>More + </Button>
-        </>
-      )}
     </>
   );
 };
--- src/features/Articles/Articles.tsx
+++ src/features/Articles/Articles.tsx
@@ -1,60 +1,91 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
 import {
-  Button,
-  Date,
+  ArticleDate,
   Description,
   Image,
   Section,
   Tile,
   Title,
+  InfoBox,
+  Box,
+  MoreButton,
 } from "./styled";
-import Country from "./Country/Country";
 import { getData } from "../../common/getData";
+import Tags from "../../common/Categories/Tags";
+import Error from "../../common/StatusPage/Error/Error";
+import Loading from "../../common/StatusPage/Loading/Loading";
+import NoResult from "../../common/StatusPage/NoResult/NoResult";
+import { searchQueryParamName } from "../../common/Navigate/SearchBar/searchQueryParamName";
 
 const Articles = () => {
-  const [limit, setLimit] = useState(10);
-  const { data, isLoading, isError } = useQuery(["articles"], getData);
+  const [searchParams] = useSearchParams();
+  const searchQuery = searchParams.get(searchQueryParamName);
 
-  const articles = data?.articles || [];
-
-  const handleShowMore = () => {
-    setLimit((prevLimit) => prevLimit + 10);
-  };
+  const { data, isLoading, isError } = useQuery(["articles", searchQuery], () =>
+    getData(searchQuery || "election")
+  );
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Loading />;
   }
 
   if (isError) {
-    return <div>Error occurred while fetching data.</div>;
+    return <Error />;
+  }
+  if (data.length === 0) {
+    return <NoResult />;
   }
 
   return (
     <>
       <Section>
-        <Country />
-        {articles.slice(0, limit).map((article: any) => (
-          <Tile key={article.id}>
-            <Title>{article.title}</Title>
-            <Date>{article.publishedAt}</Date>
-            {article.urlToImage ? (
-              <Image src={article.urlToImage} alt={article.title} />
+        <Tags />
+        {data?.map((article: any) => (
+          <Tile key={article._id}>
+            {article.multimedia && article.multimedia.length > 0 ? (
+              <Image
+                src={`https://www.nytimes.com/${article.multimedia[0].url}`}
+                alt={article.headline.main}
+              />
             ) : (
               <Image
                 src="https://t4.ftcdn.net/jpg/03/24/14/35/360_F_324143588_Jk9uwkSlhuSEyrGWkuQT7MM6mFbCayIj.jpg"
                 alt="noImage"
               />
             )}
-            <Description>{article.description}</Description>
+            <InfoBox>
+              <Title>{article.headline.main}</Title>
+              <ArticleDate>
+                {new Date(article.pub_date).toLocaleDateString("pl-PL", {
+                  day: "2-digit",
+                  month: "2-digit",
+                  year: "numeric",
+                })}
+              </ArticleDate>
+              <Description>{article.abstract}</Description>
+            </InfoBox>
+            <Box>
+              <MoreButton
+                href={article.web_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="#ad615ac1"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z" />
+                </svg>
+              </MoreButton>
+            </Box>
           </Tile>
         ))}
       </Section>
-      {limit < articles.length && (
-        <>
-          <Button onClick={handleShowMore}>More + </Button>
-        </>
-      )}
     </>
   );
 };
