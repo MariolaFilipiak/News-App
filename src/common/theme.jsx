07b7d346dda1e5d0df4bdaf8d86cd35eae60ab9b
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-07-18 09:13:44 +0200

    theme I created

---
+++ src/common/theme.jsx
@@ -0,0 +1,17 @@
+const theme = {
+    color: {
+      porcelain: "#f1f3f4",
+      ebb: "#e4dad9",
+      cornFlowerLilac :"#ffaaa3",  
+      silverChalice : "#a9a9a9",
+      doveGray: "#6a6a6a",
+      mineShaft: "#262626",
+    },
+    breakpoint: {
+      medium: "1040px",
+      mobile: "778px",
+      min: "375px",
+    },
+  };
+  export default theme;
+  
