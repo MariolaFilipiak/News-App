96ad272c983d7094b79a8f8a456a6229f2b81b08
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-07-18 09:14:04 +0200

    categories changed for nytimes

--- src/common/Categories/Categories.tsx
+++ src/common/Categories/Categories.tsx
@@ -4,13 +4,13 @@ import { Name, Wrapper } from "./styled";
 const Categories = () => {
   return (
     <Wrapper>
-      <Name to={toCategory("business")}>Business</Name>
-      <Name to={toCategory("entertainment")}>Entertainment</Name>
-      <Name to={toCategory("general")}>General</Name>
-      <Name to={toCategory("science")}>Science</Name>
+      <Name to={toCategory("Education")}>Education</Name>
+      <Name to={toCategory("Business")}>Business</Name>
+      <Name to={toCategory("World")}>World</Name>
+      <Name to={toCategory("Science")}>Science</Name>
       <Name to={toCategory("health")}> Health</Name>
       <Name to={toCategory("sports")}>Sports</Name>
-      <Name to={toCategory("technology")}>Technology</Name>
+      <Name to={toCategory("Travel")}>Travel</Name>
     </Wrapper>
   );
 };
