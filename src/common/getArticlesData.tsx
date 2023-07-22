9e8d6c4c61b1a5162890191be3b8644e6d260235
Mariola Filipiak <mfilipiak61e@gmail.com>
2023-07-22 12:56:25 +0200

    I formatted code and page added

--- src/common/getArticlesData.tsx
+++ src/common/getArticlesData.tsx
@@ -1,17 +1,21 @@
 import axios from "axios";
 
-const apiKey = "6tcR78NwMvqJfFoeHZgcDz1bQbs15Qzu"
+const apiKey = "6tcR78NwMvqJfFoeHZgcDz1bQbs15Qzu";
 
-export const getArticlesData = async (searchQuery: any) => {
+export const getArticlesData = async (searchQuery: any, page: number) => {
   const response = await axios.get(
-    `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchQuery}&sort=newest&api-key=${apiKey}`
+    `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchQuery}&sort=newest&page=${page}&api-key=${apiKey}`
   );
   return response.data.response.docs;
 };
 
-export const getCategory = async (category: string, searchQuery: any) => {
+export const getCategory = async (
+  category: string,
+  searchQuery: any,
+  page: number
+) => {
   const response = await axios.get(
-    `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchQuery}&q=${category}&sort=newest&api-key=${apiKey}`
+    `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchQuery}&q=${category}&sort=newest&page=${page}&api-key=${apiKey}`
   );
   return response.data.response.docs;
 };
